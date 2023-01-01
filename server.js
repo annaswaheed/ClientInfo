f2d73943ef51029a35c3c8a5b8e556d6c8755458
Siddi <annaswaheed@gmail.com>
2022-12-31 19:40:36 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Here my form will go')
+})
+
+
+app.get('/contact', (req, res) => {
+    res.send('contact info will be here')
+})
+
+
+app.listen(3000, () => {
+    console.log("server is running at local host 3000")
+})
